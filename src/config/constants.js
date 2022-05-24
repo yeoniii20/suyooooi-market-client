71b9c55acba4081d55c80961360205dd165542ec
yoeniii20 <commetoi_yeoni@naver.com>
2022-05-24 14:23:25 +0900

    API URL 주소 변경"

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://grab-market-server.herokuapp.com"
+    ? "https://suyooooi-market-server01.herokuapp.com"
     : "http://localhost:8080";
